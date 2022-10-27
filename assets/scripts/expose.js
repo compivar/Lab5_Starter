b8a459f96025d76653cf0a7ba4be28d8b0e9bb59
Isaac <13isaacvarsan@gmail.com>
2022-10-26 19:37:00 -0700

    Change volume to be volume slider

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -8,6 +8,32 @@ function init() {
   hornSelect.addEventListener('change', function(){
     var image = document.querySelector("img");
     image.src = "assets/images/" + `${hornSelect.value}` + ".svg";
-  
+    var audio = document.querySelector("audio");
+    audio.src = "assets/audio/" + `${hornSelect.value}` + ".mp3";
+    audio.load();
+    audio.play();
+    
   });
+
+  const volumeBar = document.getElementById("volume");
+
+  volumeBar.addEventListener('change', function () {
+    var audio = document.querySelector("audio");
+    var imgList = document.querySelectorAll("img");
+    var soundImg = imgList[1];
+    
+    if(volumeBar.value < 1) {
+      soundImg.src = "assets/icons/volume-level-0.svg";
+    } else if(volumeBar.value < 33) {
+      soundImg.src = "assets/icons/volume-level-1.svg";
+    } else if(volumeBar.value < 67) {
+      soundImg.src = "assets/icons/volume-level-2.svg";
+    } else {
+      soundImg.src = "assets/icons/volume-level-3.svg";
+    }
+    audio.volume = (volumeBar.value/100);
+    
+  });
+  
+
 }
